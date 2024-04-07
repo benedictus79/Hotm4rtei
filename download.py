dfbb815cb22015d9c657c1f6a943dc91c40372a4
Benedictus79 <da.colombia0xff@gmail.com>
2024-04-06 21:37:28 -0300

    Feat: Adicionado função de download DRM sem input de CDM

--- download.py
+++ download.py
@@ -1,11 +1,8 @@
-from concurrent.futures import ThreadPoolExecutor
-import shutil
 import subprocess
 import yt_dlp
 import re
-from pathlib import Path
 from connection import connect
-from login import requests, BeautifulSoup, course_link
+from login import requests, BeautifulSoup
 from utils import concat_path, create_folder, logger, os, random_sleep, shorten_folder_name, clear_folder_name, SilentLogger
 
 
@@ -29,7 +26,20 @@ def ytdlp_options(output_folder, session=None):
   return options
 
 
-def download_with_retries(ydl_opts, media):
+def download_with_ffmpeg(decryption_key, name_lesson, url):
+  cmd = [
+    'ffmpeg',
+    '-cenc_decryption_key', decryption_key,
+    '-headers', 'Referer: https://cf-embed.play.hotmart.com/',
+    '-y',
+    '-i', url,
+    '-codec', 'copy',
+    f"{name_lesson}.mp4"
+  ]
+  return subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def download_with_ytdlp(ydl_opts, media):
   try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
       ydl.download([media])
@@ -43,28 +53,98 @@ def process_webinar(webinar_folder, index, webinar, session):
   response = session.get(webinar).json()
   webinar_link, webinar_title = response['url'], response['name']
   webinar_folder = concat_path(webinar_folder, f'{index:03d} - {webinar_title}.mp4')
-  download_complementary(webinar_folder, webinar_link)
+  return download_complementary(webinar_folder, webinar_link)
 
 
 def process_complementary_readings(complementary_folder, complementary_url, complementarys_name, session):
   if 'youtube' in complementary_url or 'youtu.be' in complementary_url:
     new_complementary_folder = shorten_folder_name(concat_path(complementary_folder, f'{complementarys_name}.mp4'))
-    download_complementary(new_complementary_folder, complementary_url)
+    return download_complementary(new_complementary_folder, complementary_url)
   elif complementary_url:
-    save_link(complementary_folder, complementary_url, complementarys_name)
+    return save_link(complementary_folder, complementary_url, complementarys_name)
+  
+
+def get_pssh(response):
+  soup = BeautifulSoup(response.text, 'html.parser')
+  pssh_element = soup.find('cenc:pssh')
+  if pssh_element:
+    pssh = pssh_element.text
+    return pssh
 
 
-def download_iframe_video_task(output_path, video_url, session, headers=None):
-  if headers:
-    session.headers.update(headers)
-  download_complementary(output_path, video_url, session)
+def get_license(lesson_video, session):
+  params = {
+    'token': lesson_video['signature'],
+    'userCode': lesson_video['userCode'],
+    'applicationCode': lesson_video['applicationKey'],
+    }
+  data = '\b\x04'
+  headers = {
+    'accept': '*/*',
+    'accept-language': 'pt-BR,pt;q=0.7',
+    'cache-control': 'no-cache',
+    'content-type': 'application/octet-stream',
+    'dnt': '1',
+    'keysystem': 'com.widevine.alpha',
+    'membership': f'{lesson_video['clubMembershipId']}',
+    'origin': 'https://cf-embed.play.hotmart.com',
+    'pragma': 'no-cache',
+    'referer': 'https://cf-embed.play.hotmart.com/',
+  }
+  response = session.post(
+    f'https://api-player-embed.hotmart.com/v2/drm/{lesson_video['mediaCode']}/license',
+    params=params,
+    headers=headers,
+    data=data,
+  )
+  return response.url
+
+
+def get_key_drm(data):
+  api_url = "https://cdrm-project.com/api"
+  license_url = data['license']
+  pssh = data['pssh']
+  json_data = {
+    "license": license_url,
+    "headers": 'accept: "*/*"\naccept-language: "pt-BR,pt;q=0.7"\ncache-control: no-cache\ncontent-type: application/octet-stream\ndnt: "1"\nkeysystem: com.widevine.alpha\norigin: "https://cf-embed.play.hotmart.com"\npragma: no-cache\nreferer: "https://cf-embed.play.hotmart.com/"\nuser-agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"',
+    "pssh": pssh,
+    "buildInfo": "",
+    "proxy": "",
+    "cache": False,
+  }
+  headers = {
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,gl;q=0.6,es;q=0.5",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Origin": "https://cdrm-project.com",
+    "Pragma": "no-cache",
+    "Referer": "https://cdrm-project.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+  }
+  r = requests.post(api_url, json=json_data, headers=headers).json()
+  decryption_key = r['keys'][0]['key'].split(':')[1]
+  return decryption_key
 
 
 def download_video(path, index, lesson_video, session):
+  if 'drm/' in lesson_video['url']:
+    response = connect(lesson_video['url'], session)
+    pssh = get_pssh(response)
+    license = get_license(lesson_video, session)
+    wv_data = {
+      'license': license,
+      'pssh': pssh
+    }
+    decryption_key = get_key_drm(wv_data)
+    name_lesson = concat_path(path, f' {index:03} - aula')
+    return download_with_ffmpeg(decryption_key, name_lesson, lesson_video['url'])
+    
   output = shorten_folder_name(concat_path(path, f'{index:03} - aula.mp4'))
   ydl_opts = ytdlp_options(output, session)
-  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([lesson_video])
+  return download_with_ytdlp(ydl_opts, lesson_video['url'])
 
 
 def download_file(path, attachments):
@@ -108,7 +188,7 @@ def download_complementary(complementary_folder, complementary, session=None):
   ydl_opts = ytdlp_options(complementary_folder)
   if session:
     ydl_opts = ytdlp_options(complementary_folder, session)
-  download_with_retries(ydl_opts, complementary)
+  download_with_ytdlp(ydl_opts, complementary)
 
 
 def is_vimeo_iframe(iframe):
